8ac7c2e3fbf98b1dfd721c0cc1c9026a6902c40f
alonserz <59419908+alonserz@users.noreply.github.com>
2026-05-19 14:49:30 +0300

    Add task priority

--- track.c
+++ track.c
@@ -10,6 +10,17 @@
 #define DEFAULT_TASK_DIRECTORY "./tasks/"
 #define DEFAULT_TASK_FILENAME "/task.md"
 
+#define da_append(xs, x)                                                             \
+    do {                                                                             \
+        if ((xs)->count >= (xs)->capacity) {                                         \
+            if ((xs)->capacity == 0) (xs)->capacity = 256;                           \
+            else (xs)->capacity *= 2;                                                \
+            (xs)->items = realloc((xs)->items, (xs)->capacity*sizeof(*(xs)->items)); \
+        }                                                                            \
+                                                                                     \
+        (xs)->items[(xs)->count++] = (x);                                            \
+    } while (0)
+
 typedef enum {
     TASK_OPEN,
     TASK_CLOSED,
@@ -33,7 +44,15 @@ typedef struct {
     char* description;
     TaskStatus status;
     char* tags;
+    int priority;
+    char* path;
 } Task;
+
+typedef struct {
+    Task* items;
+    size_t count;
+    size_t capacity;
+} Tasks;
 
 bool mkdir_if_not_exists(char* filepath)
 {
@@ -41,8 +60,9 @@ bool mkdir_if_not_exists(char* filepath)
     if (result < 0) {
 	if (errno == EEXIST) {
 	    printf("Directory %s already exists\n", filepath);
+	} else {
+	    printf("Couldn't create directory %s: %s\n", filepath, strerror(errno));
 	}
-	printf("Couldn't create directory %s: %s\n", filepath, strerror(errno));
 	return false;
     }
     return true;
@@ -97,6 +117,14 @@ CommandType string_as_command_type(char* string)
 
 }
 
+int task_compare(const void* s1, const void* s2)
+{
+    Task* t1 = (Task*)s1;
+    Task* t2 = (Task*)s2;
+
+    return -1 * (t1->priority - t2->priority);
+}
+
 char* get_current_time()
 {
     time_t timer = time(NULL);
@@ -122,6 +150,7 @@ bool write_task_to_file(char* filepath, Task* task)
     fprintf(task_file, "%s\n", task->description);
     fprintf(task_file, "%s\n", task_status_as_string(task->status));
     fprintf(task_file, "%s\n", task->tags);
+    fprintf(task_file, "%d\n", task->priority);
 
     fclose(task_file);
     return true;
@@ -154,6 +183,9 @@ Task read_task_from_file(char* filepath)
 	if (counter == 2) {
 	    task.tags = strdup(line);
 	}
+	if (counter == 3) {
+	    task.priority = strtol(strdup(line), NULL, 10);
+	}
 	counter++;
     }
     fclose(task_file);
@@ -163,6 +195,7 @@ Task read_task_from_file(char* filepath)
 
 bool print_tasks()
 {
+    Tasks tasks = {0};
     struct dirent* de;
     DIR* dir = opendir(DEFAULT_TASK_DIRECTORY);
     if (dir == NULL) {
@@ -184,26 +217,33 @@ bool print_tasks()
 
 	Task task = read_task_from_file(path);
 	if (task.description == 0) continue; // can't properly read file 
-
-	printf("[%s] (%s) [%s] - %s\n", path, task_status_as_string(task.status), task.tags, task.description);
-
+	task.path = strdup(path);
+	da_append(&tasks, task); // add each task to dynamic array
 	free(path);
     }
+
+    qsort(tasks.items, tasks.count, sizeof(Task), task_compare);
+
+    for (size_t i = 0; i < tasks.count; i++) {
+	Task task = tasks.items[i];
+	printf("[%s] [%d] (%s) [%s] - %s\n", task.path, task.priority, task_status_as_string(task.status), task.tags, task.description);	
+    }
+
     closedir(dir); 
     return true;
 }
 
-bool create_task(char* description, char* tags)
+bool create_task(int priority, char* description, char* tags)
 {
     char* current_time = get_current_time();
-    Task task = {description, TASK_OPEN, tags};
-
     char* path = malloc(strlen(current_time) + strlen(DEFAULT_TASK_DIRECTORY) + strlen(DEFAULT_TASK_FILENAME) + 1);
+
     strcpy(path, DEFAULT_TASK_DIRECTORY);
     strcat(path, current_time);
     mkdir_if_not_exists(path);
 
     strcat(path, DEFAULT_TASK_FILENAME);
+    Task task = {description, TASK_OPEN, tags, priority, path};
     bool write_result = write_task_to_file(path, &task);
     if (!write_result) return false;
 
@@ -215,17 +255,17 @@ bool create_task(char* description, char* tags)
 void print_help()
 {
     Command commands[] = {
-	{COMMAND_CREATE, "<DESCRIPTION> <TAGS>", "creates new task with given description and tags"},
+	{COMMAND_CREATE, "<PRIORITY> <DESCRIPTION> <TAGS>", "creates new task with given priority, description and tags"},
 	{COMMAND_LS, "", "shows all available tasks"},
 	{COMMAND_INIT, "", "creates new directory `tasks` in current directory"},
     };
 
-    int commands_amount = sizeof(commands) / sizeof(commands[0]);
+    size_t commands_amount = sizeof(commands) / sizeof(commands[0]);
 
     printf("usage: track <COMMAND>\n");
     printf("available commands:\n");
 
-    for (int i = 0; i < commands_amount; i++) {
+    for (size_t i = 0; i < commands_amount; i++) {
 	printf("\t%s %s - %s\n", command_type_as_string(commands[i].type), commands[i].params, commands[i].description);
     }
 }
@@ -242,13 +282,14 @@ int main(int argc, char** argv)
 
     switch (command) {
 	case COMMAND_CREATE:
-	    if (argc != 4) {
+	    if (argc != 5) {
 		print_help();
 		return 1;
 	    }
-	    char* description = argv[2];
-	    char* tags = argv[3];
-	    bool create_task_result = create_task(description, tags); 
+	    int priority = strtol(argv[2], NULL, 10);
+	    char* description = argv[3];
+	    char* tags = argv[4];
+	    bool create_task_result = create_task(priority, description, tags); 
 	    if (!create_task_result) return 1;
 	    break;
 	case COMMAND_LS:
